ba1b73bbf1eeb0a164bbb1df88d12faf515421b6
lucaxy <lucaxy@qq.com>
2019-01-22 20:13:55 +0800

    add captcha

---
+++ captcha/HansClickCaptcha.php
@@ -0,0 +1,213 @@
+<?php
+
+class HansClickCaptcha
+{
+	function __construct(){}
+
+	public function create()
+	{
+		$imageName=mt_rand(1,10);
+		$imageDirPath = dirname(__FILE__).'/image/';
+		$imagePath = $imageDirPath.$imageName.'.jpg';
+		$imageReversePath = $imageDirPath.$imageName.'_.jpg';
+		$fontPath = dirname(__FILE__).'/font/DroidSansChinese.ttf';
+		foreach ($this->randChars() as $v) {
+			$fontSize = mt_rand(15, 25);
+			//字符串文本框宽度和长度
+			$fontarea = imagettfbbox($fontSize, 0, $fontPath, $v);
+			$textWidth = $fontarea[2] - $fontarea[0];
+			$textHeight = $fontarea[1] - $fontarea[7];
+			$tmp['text'] = $v;
+			$tmp['size'] = $fontSize;
+			$tmp['width'] = $textWidth;
+			$tmp['height'] = $textHeight;
+			$textArr['text'][] = $tmp;
+		}
+		//图片宽高和类型
+		list($imageWidth, $imageHeight, $imageType) = getimagesize($imagePath);
+		$textArr['width'] = $imageWidth;
+		$textArr['height'] = $imageHeight;
+		//随机生成汉字位置
+		foreach ($textArr['text'] as &$v) {
+			list($x, $y) = $this->randPosition($textArr['text'], $imageWidth, $imageHeight, $v['width'], $v['height']);
+			$v['x'] = $x;
+			$v['y'] = $y;
+			$v['angle'] =mt_rand(-45,45);
+		}
+		unset($v);
+		//创建图片的实例
+		$image = imagecreatefromstring(file_get_contents($imagePath));
+		imagealphablending($image , false);
+		$color = imagecolorallocatealpha($image , 0 , 0 , 0 , 127);
+		foreach ($textArr['text'] as $v) {
+//			list($r, $g, $b) = $this->getImageColor($imagePath, $v['x'] + $v['width'] / 2, $v['y'] - $v['height'] / 2);
+			//字体颜色
+//			$color = imagecolorallocate($image, abs(255-$r), abs(255-$g), abs(255-$b));
+			imagettftext($image, $v['size'], $v['angle'], $v['x'], $v['y'], $color, $fontPath, $v['text']);
+		}
+		imagesavealpha($image , true);
+		$realImage=imagecreatefromstring(file_get_contents($imageReversePath));
+		imagecopy( $realImage, $image , 0 , 0 , 0 , 0 , $imageWidth , $imageHeight );
+		$textArr['text']=array_splice($textArr['text'],0,3);
+		$_SESSION['hans_click_captcha'] = $textArr;
+		ob_start();
+		imagepng($realImage);
+		imagedestroy($image);
+		imagedestroy($realImage);
+		return 'data:image/png;base64,' . base64_encode(ob_get_clean());
+	}
+
+	public function check($info, $unset = true)
+	{
+		$flag = true;
+		if (isset($_SESSION['hans_click_captcha'])) {
+			$textArr = $_SESSION['hans_click_captcha'];
+			list($xy, $w, $h) = explode(';', $info);
+			$xyArr = explode('-', $xy);
+			$xpro = $w / $textArr['width'];//宽度比例
+			$ypro = $h / $textArr['height'];//高度比例
+			foreach ($xyArr as $k => $v) {
+				$xy = explode(',', $v);
+				$x = $xy[0];
+				$y = $xy[1];
+				$xPos=$x / $xpro;
+				$yPos=$y / $ypro;
+				$angle=deg2rad($textArr['text'][$k]['angle']);
+				$xZeroNew=$textArr['text'][$k]['x']*cos($angle)-$textArr['text'][$k]['y']*sin($angle);
+				$yZeroNew=$textArr['text'][$k]['x']*sin($angle)+$textArr['text'][$k]['y']*cos($angle);
+				$xPosNew=$xPos*cos($angle)-$yPos*sin($angle);
+				$yPosNew=$xPos*sin($angle)+$yPos*cos($angle);
+				if ($xPosNew < $xZeroNew || $xPosNew > $xZeroNew + $textArr['text'][$k]['width']) {
+					$flag = false;
+					break;
+				}
+				if ($yPosNew < $yZeroNew - $textArr['text'][$k]['height'] || $yPosNew > $yZeroNew) {
+					$flag = false;
+					break;
+				}
+			}
+			if ($unset) {
+				unset($_SESSION['hans_click_captcha']);
+			}
+		} else {
+			$flag = false;
+		}
+		return $flag;
+	}
+
+	//随机生成中文汉字
+	private function randChars($length = null)
+	{
+		$length=is_null($length)?mt_rand(4,6):$length;
+		/**
+		 * 字符串截取，支持中文和其他编码
+		 * @static
+		 * @access public
+		 * @param string $str 需要转换的字符串
+		 * @param string $start 开始位置
+		 * @param string $length 截取长度
+		 * @param string $charset 编码格式
+		 * @param string $suffix 截断显示字符
+		 * @return string
+		 */
+		function msubstr($str, $start = 0, $length, $charset = 'utf-8', $suffix = true)
+		{
+			if (function_exists('mb_substr')) {
+				$slice = mb_substr($str, $start, $length, $charset);
+			} else if (function_exists('iconv_substr')) {
+				$slice = iconv_substr($str, $start, $length, $charset);
+			} else {
+				$re['utf-8'] = '/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|[\xe0-\xef][\x80-\xbf]{2}|[\xf0-\xff][\x80-\xbf]{3}/';
+				$re['gb2312'] = '/[\x01-\x7f]|[\xb0-\xf7][\xa0-\xfe]/';
+				$re['gbk'] = '/[\x01-\x7f]|[\x81-\xfe][\x40-\xfe]/';
+				$re['big5'] = '/[\x01-\x7f]|[\x81-\xfe]([\x40-\x7e]|\xa1-\xfe])/';
+				preg_match_all($re[$charset], $str, $match);
+				$slice = join('', array_slice($match[0], $start, $length));
+			}
+			return $suffix ? $slice . '...' : $slice;
+		}
+
+		$chars = '们以我到他会作时要动国产的一是工就年阶义发成部民可出能方进在了不和有大这主中人上为来分生对于学下级地个用同行面说种过命度革而多子后自社加小机也经力线本电高量长党得实家定深法表着水理化争现所二起政三好十战无农使性前等反体合斗路图把结第里正新开论之物从当两些还天资事队批点育重其思与间内去因件日利相由压员气业代全组数果期导平各基或月毛然如应形想制心样干都向变关问比展那它最及外没看治提五解系林者米群头意只明四道马认次文通但条较克又公孔领军流入接席位情运器并飞原油放立题质指建区验活众很教决特此常石强极土少已根共直团统式转别造切九你取西持总料连任志观调七么山程百报更见必真保热委手改管处己将修支识病象几先老光专什六型具示复安带每东增则完风回南广劳轮科北打积车计给节做务被整联步类集号列温装即毫知轴研单色坚据速防史拉世设达尔场织历花受求传口断况采精金界品判参层止边清至万确究书术状厂须离再目海交权且儿青才证低越际八试规斯近注办布门铁需走议县兵固除般引齿千胜细影济白格效置推空配刀叶率述今选养德话查差半敌始片施响收华觉备名红续均药标记难存测士身紧液派准斤角降维板许破述技消底床田势端感往神便贺村构照容非搞亚磨族火段算适讲按值美态黄易彪服早班麦削信排台声该击素张密害侯草何树肥继右属市严径螺检左页抗苏显苦英快称坏移约巴材省黑武培著河帝仅针怎植京助升王眼她抓含苗副杂普谈围食射源例致酸旧却充足短划剂宣环落首尺波承粉践府鱼随考刻靠够满夫失包住促枝局菌杆周护岩师举曲春元髯髰髱髲髳髴髵髶髷髸髹髺髻髼髽髾髿鬀鬁鬂鬃鬄鬅鬆鬇鬈鬉鬊鬋鬌鬍鬎鬏鬐鬑鬒鬓鬔鬕鬖鬗鬘鬙鬚鬛鬜鬝鬞鬟鬠鬡鬢鬣鬤鬥鬦鬧鬨鬩鬪鬫鬬鬭鬮鬯鬰鬱鬲鬳鬴鬵鬶鬷鬸鬹鬺鬻鬼鬽鬾鬿魀魁魂魃魄魅魆魇魈魉魊魋魌魍魎魏魐魑魒魓魔魕魖魗魘魙魚魛魜魝魞魟魠魡魢魣魤魥魦魧魨魩魪魫魬魭魮魯魰魱魲魳魴魵魶魷魸魹魺魻魼魽魾魿鮀鮁鮂鮃鮄鮅鮆鮇鮈鮉鮊鮋鮌鮍鮎鮏鮐鮑鮒鮓鮔鮕鮖鮗鮘鮙鮚鮛鮜鮝鮞鮟鮠鮡鮢鮣鮤鮥鮦鮧鮨鮩鮪鮫鮬鮭鮮鮯鮰鮱鮲鮳鮴鮵鮶鮷鮸鮹鮺鮻鮼鮽鮾鮿鯀鯁鯂鯃鯄鯅鯆鯇鯈鯉鯊鯋鯌鯍鯎鯏鯐鯑鯒鯓鯔鯕鯖鯗鯘鯙鯚鯛鯜鯝鯞鯟鯠鯡鯢鯣鯤鯥鯦鯧鯨鯩鯪鯫鯬鯭鯮鯯鯰鯱鯲鯳鯴鯵鯶鯷鯸鯹鯺鯻鯼鯽鯾鯿鰀鰁鰂鰃鰄鰅鰆鰇鰈鰉鰊鰋鰌鰍鰎鰏鰐鰑鰒鰓鰔鰕鰖鰗鰘鰙鰚鰛鰜鰝鰞鰟鰠鰡鰢鰣鰤鰥鰦鰧鰨鰩鰪鰫鰬鰭鰮鰯鰰鰱鰲鰳鰴鰵鰶鰷鰸鰹鰺鰻鰼鰽鰾鰿鱀鱁鱂鱃鱄鱅鱆鱇鱈鱉鱊鱋鱌鱍鱎鱏鱐鱑鱒鱓鱔鱕鱖鱗鱘鱙鱚鱛鱜鱝鱞鱟鱠鱡鱢鱣鱤鱥鱦鱧鱨鱩鱪鱫鱬鱭鱮鱯鱰鱱鱲鱳鱴鱵鱶鱷鱸鱹鱺鱻鱼鱽鱾鱿鲀鲁鲂鲃鲄鲅鲆鲇鲈鲉鲊鲋鲌鲍鲎鲏鲐鲑鲒鲓鲔鲕鲖鲗鲘鲙鲚鲛鲜鲝鲞鲟鲠鲡鲢鲣鲤鲥鲦鲧鲨鲩鲪鲫鲬鲭鲮鲯鲰鲱鲲鲳鲴鲵鲶鲷鲸鲹鲺鲻鲼鲽鲾鲿鳀鳁鳂鳃鳄鳅鳆鳇鳈鳉鳊鳋鳌鳍鳎鳏鳐鳑鳒鳓鳔鳕鳖鳗鳘鳙鳚鳛鳜鳝鳞鳟鳠鳡鳢鳣鳤鳥鳦鳧鳨鳩鳪鳫鳬鳭鳮鳯鳰鳱鳲鳳鳴鳵鳶鳷鳸鳹鳺鳻鳼鳽鳾鳿鴀鴁鴂鴃鴄鴅鴆鴇鴈鴉鴊鴋鴌鴍鴎鴏鴐鴑鴒鴓鴔鴕鴖鴗鴘鴙鴚鴛鴜鴝鴞鴟鴠鴡鴢鴣鴤鴥鴦鴧鴨鴩鴪鴫鴬鴭鴮鴯鴰鴱鴲鴳鴴鴵鴶鴷鴸鴹鴺鴻鴼鴽鴾鴿鵀鵁鵂鵃鵄鵅鵆鵇鵈鵉鵊鵋鵌鵍鵎鵏鵐鵑鵒鵓鵔鵕鵖鵗鵘鵙鵚鵛鵜鵝鵞鵟鵠鵡鵢鵣鵤鵥鵦鵧鵨鵩鵪鵫鵬鵭鵮鵯鵰鵱鵲鵳鵴鵵鵶鵷鵸鵹鵺鵻鵼鵽鵾鵿鶀鶁鶂鶃鶄鶅鶆鶇鶈鶉鶊鶋鶌鶍鶎鶏鶐鶑鶒鶓鶔鶕鶖鶗鶘鶙鶚鶛鶜鶝鶞鶟鶠鶡鶢鶣鶤鶥鶦鶧鶨鶩鶪鶫鶬鶭鶮鶯鶰鶱鶲鶳鶴鶵鶶鶷鶸鶹鶺鶻鶼鶽鶾鶿鷀鷁鷂鷃鷄鷅鷆鷇鷈鷉鷊鷋鷌鷍鷎鷏鷐鷑鷒鷓鷔鷕鷖鷗鷘鷙鷚鷛鷜鷝鷞鷟鷠鷡鷢鷣鷤鷥鷦鷧鷨鷩鷪鷫鷬鷭鷮鷯鷰鷱鷲鷳鷴鷵鷶鷷鷸鷹鷺鷻鷼鷽鷾鷿鸀鸁鸂鸃鸄鸅鸆鸇鸈鸉鸊鸋鸌鸍鸎鸏鸐鸑鸒鸓鸔鸕鸖鸗鸘鸙鸚鸛鸜鸝鸞鸟鸠鸡鸢鸣鸤鸥鸦鸧鸨鸩鸪鸫鸬鸭鸮鸯鸰鸱鸲鸳鸴鸵鸶鸷鸸鸹鸺鸻鸼鸽鸾鸿鹀鹁鹂鹃鹄鹅鹆鹇鹈鹉鹊鹋鹌鹍鹎鹏鹐鹑鹒鹓鹔鹕鹖鹗鹘鹙鹚鹛鹜鹝鹞鹟鹠鹡鹢鹣鹤鹥鹦鹧鹨鹩鹪鹫鹬鹭鹮鹯鹰鹱鹲鹳鹴鹵鹶鹷鹸鹹鹺鹻鹼鹽鹾鹿麀麁麂麃麄麅麆麇麈麉麊麋麌麍麎麏麐麑麒麓麔麕麖麗麘麙麚麛麜麝麞麟麠麡麢麣麤麥麦麧麨麩麪麫麬麭麮麯麰麱麲麳麴麵麶麷麸麹麺麻麼麽麾麿黀黁黂黃黄黅黆黇黈黉黊黋黌黍黎黏黐黑黒黓黔黕黖黗默黙黚黛黜黝點黟黠黡黢黣黤黥黦黧黨黩黪黫黬黭黮黯黰黱黲黳黴黵黶黷黸黹黺黻黼黽黾黿鼀鼁鼂鼃鼄鼅鼆鼇鼈鼉鼊鼋鼌鼍鼎鼏鼐鼑鼒鼓鼔鼕鼖鼗鼘鼙鼚鼛鼜鼝鼞鼟鼠鼡鼢鼣鼤鼥鼦鼧鼨鼩鼪鼫鼬鼭鼮鼯鼰鼱鼲鼳鼴鼵鼶鼷鼸鼹鼺鼻鼼鼽鼾鼿齀齁齂齃齄齅齆齇齈齉齊齋齌齍齎齏齐齑齒齓齔齕齖齗齘齙齚齛齜齝齞齟齠齡齢齣齤齥齦齧齨齩齪齫齬齭齮齯齰齱齲齳齴齵齶齷齸齹齺齻齼齽齾齿龀龁龂龃龄龅龆龇龈龉龊龋龌龍龎龏龐龑龒龓龔龕龖龗龘龙龚龛龜龝龞龟龠龡龢龣龤龥超负砂封换太模贫减阳扬江析亩木言球朝医校古呢稻宋听唯输滑站另卫字鼓刚写刘微略范供阿块某功套友限项余倒卷创律雨让骨远帮初皮播优占死毒圈伟季训控激找叫云互跟裂粮粒母练塞钢顶策双留误础吸阻故寸盾晚丝女散焊功株亲院冷彻弹错散商视艺灭版烈零室轻血倍缺厘泵察绝富城冲喷壤简否柱李望盘磁雄似困巩益洲脱投送奴侧润盖挥距触星松送获兴独官混纪依未突架宽冬章湿偏纹吃执阀矿寨责熟稳夺硬价努翻奇甲预职评读背协损棉侵灰虽矛厚罗泥辟告卵箱掌氧恩爱停曾溶营终纲孟钱待尽俄缩沙退陈讨奋械载胞幼哪剥迫旋征槽倒握担仍呀鲜吧卡粗介钻逐弱脚怕盐末阴丰雾冠丙街莱贝辐肠付吉渗瑞惊顿挤秒悬姆烂森糖圣凹陶词迟蚕亿矩康遵牧遭幅园腔订香肉弟屋敏恢忘编印蜂急拿扩伤飞露核缘游振操央伍域甚迅辉异序免纸夜乡久隶缸夹念兰映沟乙吗儒杀汽磷艰晶插埃燃欢铁补咱芽永瓦倾阵碳演威附牙芽永瓦斜灌欧献顺猪洋腐请透司危括脉宜笑若尾束壮暴企菜穗楚汉愈绿拖牛份染既秋遍锻玉夏疗尖殖井费州访吹荣铜沿替滚客召旱悟刺脑措贯藏敢令隙炉壳硫煤迎铸粘探茰茱茲茳茴茵茶茷茸茹茺茻茼茽茾茿荀荁荂荃荄荅荆荇荈草荊荋荌荍荎荏荐荑荒荓荔荕荖荗荘荙荚荛荜荝荞荟荠荡荢荣荤荥荦荧荨荩荪荫荬荭荮药荰荱荲荳荴荵荶荷荸荹荺荻荼荽荾荿莀莁莂莃莄莅莆莇莈莉莊莋莌莍莎莏莐莑莒莓莔莕莖莗莘莙莚莛莜莝莞莟莠莡莢莣莤莥莦莧莨莩莪莫莬莭莮莯莰莱莲莳莴莵莶获莸莹莺莻莼莽莾莿菀菁菂菃菄菅菆菇菈菉菊菋菌菍菎菏菐菑菒菓菔菕菖菗菘菙菚菛菜菝菞菟菠菡菢菣菤菥菦菧菨菩菪菫菬菭菮華菰菱菲菳菴菵菶菷菸菹菺菻菼菽菾菿萀萁萂萃萄萅萆萇萈萉萊萋萌萍萎萏萐萑萒萓萔萕萖萗萘萙萚萛萜萝萞萟萠萡萢萣萤营萦萧萨萩萪萫萬萭萮萯萰萱萲萳萴萵萶萷萸萹萺萻萼落萾萿葀葁葂葃葄葅葆葇葈葉葊葋葌葍葎葏葐葑葒葓葔葕葖著葘葙葚葛葜葝葞葟葠葡葢董葤葥葦葧葨葩葪葫葬葭葮葯葰葱葲葳葴葵葶葷葸葹葺葻葼葽葾葿蒀蒁蒂蒃蒄蒅蒆蒇蒈蒉蒊蒋蒌蒍蒎蒏蒐蒑蒒蒓蒔蒕蒖蒗蒘蒙蒚蒛蒜蒝蒞蒟蒠蒡蒢蒣蒤蒥蒦蒧蒨蒩蒪蒫蒬蒭蒮蒯蒰蒱蒲蒳蒴蒵蒶蒷蒸蒹蒺蒻蒼蒽蒾蒿蓀蓁蓂蓃蓄蓅蓆蓇蓈蓉蓊蓋蓌蓍蓎蓏蓐蓑蓒蓓蓔蓕蓖蓗蓘蓙蓚蓛蓜蓝蓞蓟蓠蓡蓢蓣蓤蓥蓦蓧蓨蓩蓪蓫蓬蓭蓮蓯蓰蓱蓲蓳蓴蓵蓶蓷蓸蓹蓺蓻蓼蓽蓾蓿蔀蔁蔂蔃蔄蔅蔆蔇蔈蔉蔊蔋蔌蔍蔎蔏蔐蔑蔒蔓蔔蔕蔖蔗蔘蔙蔚蔛蔜蔝蔞蔟蔠蔡蔢蔣蔤蔥蔦蔧蔨蔩蔪蔫蔬蔭蔮蔯蔰蔱蔲蔳蔴蔵蔶蔷蔸蔹蔺蔻蔼蔽蔾蔿蕀蕁蕂蕃蕄蕅蕆蕇蕈蕉蕊蕋蕌蕍蕎蕏蕐蕑蕒蕓蕔蕕蕖蕗蕘蕙蕚蕛蕜蕝蕞蕟蕠蕡蕢蕣蕤蕥蕦蕧蕨蕩蕪蕫蕬蕭蕮蕯蕰蕱蕲蕳蕴蕵蕶蕷蕸蕹蕺蕻蕼蕽蕾蕿薀薁薂薃薄薅薆薇薈薉薊薋薌薍薎薏薐薑薒薓薔薕薖薗薘薙薚薛薜薝薞薟薠薡薢薣薤薥薦薧薨薩薪薫薬薭薮薯薰薱薲薳薴薵薶薷薸薹薺薻薼薽薾薿藀藁藂藃藄藅藆藇藈藉藊藋藌藍藎藏藐藑藒藓藔藕藖藗藘藙藚藛藜藝藞藟藠藡藢藣藤藥藦藧藨藩藪藫藬藭藮藯藰藱藲藳藴藵藶藷藸藹藺藻藼藽藾藿蘀蘁蘂蘃蘄蘅蘆蘇蘈蘉蘊蘋蘌蘍蘎蘏蘐蘑蘒蘓蘔蘕蘖蘗蘘蘙蘚蘛蘜蘝蘞蘟蘠蘡蘢蘣蘤蘥蘦蘧蘨蘩蘪蘫蘬蘭蘮蘯蘰蘱蘲蘳蘴蘵蘶蘷蘸蘹蘺蘻蘼蘽蘾蘿虀虁虂虃虄虅虆虇虈虉虊虋虌虍虎虏虐虑虒虓虔處虖虗虘虙虚虛虜虝虞號虠虡虢虣虤虥虦虧虨虩虪虫虬虭虮虯虰虱虲虳虴虵虶虷虸虹虺虻虼虽虾虿蚀蚁蚂蚃蚄蚅蚆蚇蚈蚉蚊蚋蚌蚍蚎蚏蚐蚑蚒蚓蚔蚕蚖蚗蚘蚙蚚蚛蚜蚝蚞蚟蚠蚡蚢蚣蚤蚥蚦蚧蚨蚩蚪蚫蚬蚭蚮蚯蚰蚱蚲蚳蚴蚵蚶蚷蚸蚹蚺蚻蚼蚽蚾蚿蛀蛁蛂蛃蛄蛅蛆蛇蛈蛉蛊蛋蛌蛍蛎蛏蛐蛑蛒蛓蛔蛕蛖蛗蛘蛙蛚蛛蛜蛝蛞蛟蛠蛡蛢蛣蛤蛥蛦蛧蛨蛩蛪蛫蛬蛭蛮蛯蛰蛱蛲蛳蛴蛵蛶蛷蛸蛹蛺蛻蛼蛽蛾蛿蜀蜁蜂蜃蜄蜅蜆蜇蜈蜉蜊蜋蜌蜍蜎蜏蜐蜑蜒蜓蜔蜕蜖蜗蜘蜙蜚蜛蜜蜝蜞蜟蜠蜡蜢蜣蜤蜥蜦蜧蜨蜩蜪蜫蜬蜭蜮蜯蜰蜱蜲蜳蜴蜵蜶蜷蜸蜹蜺蜻蜼蜽蜾蜿蝀蝁蝂蝃蝄蝅蝆蝇蝈蝉蝊蝋蝌蝍蝎蝏蝐蝑蝒蝓蝔蝕蝖蝗蝘蝙蝚蝛蝜蝝蝞蝟蝠蝡蝢蝣蝤蝥蝦蝧蝨蝩蝪蝫蝬蝭蝮蝯蝰蝱蝲蝳蝴蝵蝶蝷蝸蝹蝺蝻蝼蝽蝾蝿螀螁螂螃螄螅螆螇螈螉螊螋螌融螎螏螐螑螒螓螔螕螖螗螘螙螚螛螜螝螞螟螠螡螢螣螤螥螦螧螨螩螪螫螬螭螮螯螰螱螲螳螴螵螶螷螸螹螺螻螼螽螾螿蟀蟁蟂蟃蟄蟅蟆蟇蟈蟉蟊蟋蟌蟍蟎蟏蟐蟑蟒蟓蟔蟕蟖蟗蟘蟙蟚蟛蟜蟝蟞蟟蟠蟡蟢蟣蟤蟥蟦蟧蟨蟩蟪蟫蟬蟭蟮蟯蟰蟱蟲蟳蟴蟵蟶蟷蟸蟹蟺蟻蟼蟽蟾蟿蠀蠁蠂蠃蠄蠅蠆蠇蠈蠉蠊蠋蠌蠍蠎蠏蠐蠑蠒蠓蠔蠕蠖蠗蠘蠙蠚蠛蠜蠝蠞蠟蠠蠡蠢蠣蠤蠥蠦蠧蠨蠩蠪蠫蠬蠭蠮蠯蠰蠱蠲蠳蠴蠵蠶蠷蠸蠹蠺蠻蠼蠽蠾蠿血衁衂衃衄衅衆衇衈衉衊衋行衍衎衏衐衑衒術衔衕衖街衘衙衚衛衜衝衞衟衠衡衢衣衤补衦衧表衩衪衫衬衭衮衯衰衱衲衳衴衵衶衷衸衹衺衻衼衽衾衿袀袁袂袃袄袅袆袇袈袉袊袋袌袍袎袏袐袑袒袓袔袕袖袗袘袙袚袛袜袝袞袟袠袡袢袣袤袥袦袧袨袩袪被袬袭袮袯袰袱袲袳袴袵袶袷袸袹袺袻袼袽袾袿裀裁裂裃裄装裆裇裈裉裊裋裌裍裎裏裐裑裒裓裔裕裖裗裘裙裚裛補裝裞裟裠裡裢裣裤裥裦裧裨裩裪裫裬裭裮裯裰裱裲裳裴裵裶裷裸裹裺裻裼製裾裿褀褁褂褃褄褅褆複褈褉褊褋褌褍褎褏褐褑褒褓褔褕褖褗褘褙褚褛褜褝褞褟褠褡褢褣褤褥褦褧褨褩褪褫褬褭褮褯褰褱褲褳褴褵褶褷褸褹褺褻褼褽褾褿襀襁襂襃襄襅襆襇襈襉襊襋襌襍襎襏襐襑襒襓襔襕襖襗襘襙襚襛襜襝襞襟襠襡襢襣襤襥襦襧襨襩襪襫襬襭襮襯襰襱襲襳襴襵襶襷襸襹襺襻襼襽襾西覀要覂覃覄覅覆覇覈覉覊見挖氏刃剧堆赫荷胸衡勤膜篇登驻案刊秧缓凸役剪川雪链渔啦脸户洛孢勃盟买杨宗焦赛旗滤硅炭股坐蒸凝竟陷枪黎救冒暗洞犯筒您宋弧爆谬涂味津臂障褐陆啊健尊豆拔莫抵桑坡缝警挑污冰柬嘴啥饭塑寄赵喊垫丹渡耳刨虎笔稀昆浪萨茶滴浅拥穴覆伦娘吨浸袖珠雌妈紫戏塔锤震岁貌洁剖牢锋疑霸闪埔猛诉刷狠忽灾闹乔唐漏闻沈熔氯荒茎男凡抢像浆旁玻亦忠唱蒙予纷捕锁尤乘乌智淡允叛畜俘摸锈扫毕璃宝芯爷鉴秘净蒋钙肩腾枯抛轨堂拌爸循诱祝励肯酒绳穷塘燥泡袋朗喂铝软渠颗惯贸粪综墙趋彼届墨碍启逆卸航衣孙龄岭骗休借一掌控全面精准管理™';
+		for ($i = 0; $i < $length; $i++) {
+			$return[] = msubstr($chars, floor(mt_rand(0, mb_strlen($chars, 'utf-8') - 1)), 1, 'utf-8', false);
+		}
+		return $return;
+	}
+
+	//随机生成位置布局
+	private function randPosition($textArr, $imgW, $imgH, $fontW, $fontH)
+	{
+		$return = array();
+		$x = mt_rand(0, $imgW - $fontW);
+		$y = mt_rand($fontH, $imgH);
+		//碰撞验证
+		if (!$this->checkPosition($textArr, $x, $y, $fontW, $fontH)) {
+			$return = $this->randPosition($textArr, $imgW, $imgH, $fontW, $fontH);
+		} else {
+			$return = array($x, $y);
+		}
+		return $return;
+	}
+
+	private function checkPosition($textArr, $x, $y, $w, $h)
+	{
+		$flag = true;
+		foreach ($textArr as $v) {
+			if (isset($v['x']) && isset($v['y'])) {
+				//分别判断X和Y是否都有交集，如果都有交集，则判断为覆盖
+				$flagX = true;
+				if ($v['x'] > $x) {
+					if ($x + $w > $v['x']) {
+						$flagX = false;
+					}
+				} else if ($x > $v['x']) {
+					if ($v['x'] + $v['width'] > $x) {
+						$flagX = false;
+					}
+				} else {
+					$flagX = false;
+				}
+				$flagY = true;
+				if ($v['y'] > $y) {
+					if ($y + $h > $v['y']) {
+						$flagY = false;
+					}
+				} else if ($y > $v['y']) {
+					if ($v['y'] + $v['height'] > $y) {
+						$flagY = false;
+					}
+				} else {
+					$flagY = false;
+				}
+				if (!$flagX && !$flagY) {
+					$flag = false;
+				}
+			}
+		}
+		return $flag;
+	}
+
+	//获取图片某个定点上的主要色
+	private function getImageColor($img, $x, $y)
+	{
+		list($imageWidth, $imageHeight, $imageType) = getimagesize($img);
+		switch ($imageType) {
+			case 1://GIF
+				$im = imagecreatefromgif($img);
+				break;
+			case 2://JPG
+				$im = imagecreatefromjpeg($img);
+				break;
+			case 3://PNG
+				$im = imagecreatefrompng($img);
+				break;
+		}
+		$rgb = imagecolorat($im, $x, $y);
+		$r = ($rgb >> 16) & 0xFF;
+		$g = ($rgb >> 8) & 0xFF;
+		$b = $rgb & 0xFF;
+		return array($r, $g, $b);
+	}
+}
+
+?>
---
+++ captcha/hans_click_captcha.vue
@@ -0,0 +1,232 @@
+<template>
+    <div>
+        <Form-item v-if="captchaType=='slide'">
+            <div id="geetestCaptcha">
+                <p id="geetestWait" class="geetest-show">正在加载验证码......</p>
+            </div>
+        </Form-item>
+        <Form-item v-if="captchaType=='img'">
+            <Row type="flex" align="middle">
+                <Col span="8">
+                <Input type="text" v-model="formItem.captcha" @input="updateCaptchaValue" placeholder="验证码">
+                <Icon type="ios-barcode-outline" slot="prepend"></Icon>
+                </Input>
+                </Col>
+                <Col span="12" style="text-align: center;height: 40px;">
+                <img :src="captchaImg" alt="验证码" title="点击刷新！" @click="fetchNewCaptchaImg">
+                </Col>
+                <Col span="4"><Button type="ghost" shape="circle" icon="android-refresh" @click="fetchNewCaptchaImg"></Button></Col>
+            </Row>
+        </Form-item>
+        <Form-item v-if="captchaType==='click'">
+            <Row>
+                <Col span="24">
+                <div :style="{backgroundImage: 'url('+captchaImg+')',backgroundSize:'cover',paddingBottom:'56.8%'}">
+                    <div ref="click-captcha-container" style="position: absolute;width: 100%;height: 100%;top:0;left: 0;" @click="handleCaptchaClick($event)">
+                        <Button shape="circle" @click.stop="fetchNewCaptchaImg" icon="refresh" style="position: absolute;top:0;right: 0;"></Button>
+                        <Icon v-if="clickCaptchaTextColor[0]==='#fff'" type="heart" :size="35" :color="'#ed3f14'" :style="{position: 'absolute',top:(Number(clickCaptchaInfo[0].split(',')[1])-17)+'px',left: (Number(clickCaptchaInfo[0].split(',')[0])-14)+'px'}"></Icon>
+                        <Icon v-if="clickCaptchaTextColor[1]==='#fff'" type="heart" :size="35" :color="'#ed3f14'" :style="{position: 'absolute',top:(Number(clickCaptchaInfo[1].split(',')[1])-17)+'px',left: (Number(clickCaptchaInfo[1].split(',')[0])-14)+'px'}"></Icon>
+                        <Icon v-if="clickCaptchaTextColor[2]==='#fff'" type="heart" :size="35" :color="'#ed3f14'" :style="{position: 'absolute',top:(Number(clickCaptchaInfo[2].split(',')[1])-17)+'px',left: (Number(clickCaptchaInfo[2].split(',')[0])-14)+'px'}"></Icon>
+                    </div>
+                </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col :style="{background: clickCaptchaBarColor,color:'#fff',textAlign:'center',marginTop:'5px',fontSize:'20px'}" span="24">
+                {{clickCaptchaTip}}：
+                <span v-for="(item,index) in clickCaptchaTextArr" :key="index">{{index!==0?'、':''}}&nbsp;"<span :style="{color:clickCaptchaTextColor[index]}">{{item}}</span>"&nbsp;</span>
+                </Col>
+            </Row>
+        </Form-item>
+    </div>
+</template>
+<script>
+	import util from '../libs/util';
+	import qs from 'qs';
+    export default {
+        data(){
+            return {
+	            formItem: {
+		            captcha:'',
+	            },
+	            captchaType:this.$store.state.server_data.captchaType,
+	            captchaImg:this.$store.state.captcha_data.captchaImg,
+	            clickCaptchaTextArr:this.$store.state.captcha_data.captchaText,
+	            clickCaptchaTip:'请依次点击上图中',
+	            clickCaptchaInfo:[],
+	            clickCaptchaBarColor:'#5cadff',
+	            clickCaptchaBarColorOri:'#5cadff',
+	            clickCaptchaBarColorSuccess:'#19be6b',
+	            clickCaptchaBarColorFailure:'#ed3f14',
+	            clickCaptchaTextColor:this.$store.state.captcha_data.captchaText.map(function(){
+		            return '#000';
+	            }),
+	            destroyed:false
+            }
+        },
+	    mounted(){
+		    let thisApp=this;
+		    if(this.captchaType==='slide'){
+			    this.showSpin=true;
+			    util.loadScript('/index_org/assets/gt.js',function () {
+				    util.ajax.get("/lib/StartCaptchaServlet.php?t="+ (new Date()).getTime())
+					    .then(function(res){
+						    thisApp.showSpin=false;
+						    if(thisApp.destroyed) return;
+						    window.initGeetest({
+							    gt: res.data.gt,
+							    challenge: res.data.challenge,
+							    product: "popup",
+							    offline: !res.data.success,
+							    new_captcha: res.data.new_captcha,
+							    width:'100%',
+						    }, function(captchaObj){
+							    if(!thisApp.destroyed)
+							        captchaObj.appendTo('#geetestCaptcha');
+							    else
+							    	return ;
+							    captchaObj.onReady(function () {
+								    document.querySelector('#geetestWait').style.display='none';
+							    });
+							    captchaObj.onSuccess(function () {
+                                    thisApp.updateCaptchaValue();
+							    });
+							    captchaObj.onError(function () {
+								    thisApp.updateCaptchaValue();
+							    });
+							    window.gt = captchaObj;
+						    });
+					    })
+					    .catch(function(err){
+						    console.log(err);
+					    })
+			    });
+		    }
+	    },
+        methods:{
+        	reset:function () {
+		        if(this.captchaType==='slide'){
+			        window.gt.reset();
+			        this.updateCaptchaValue();
+                }else{
+		        	this.fetchNewCaptchaImg();
+                }
+	        },
+            destroy:function () {
+                this.destroyed=true;
+            },
+	        updateCaptchaValue:function () {
+		        let result;
+		        if(this.captchaType==='slide'){
+			        let gtResult = window.gt.getValidate();
+			        if(gtResult){
+				        result={
+					        'geetest_challenge':gtResult.geetest_challenge,
+					        'geetest_validate':gtResult.geetest_validate,
+					        'geetest_seccode':gtResult.geetest_seccode,
+				        };
+                    }else{
+			        	result='';
+                    }
+		        }else{
+		        	if(this.formItem.captcha){
+				        result = {
+					        captcha:this.formItem.captcha
+				        };
+                    }else{
+				        result='';
+                    }
+		        }
+		        this.$emit('change', result);
+	        },
+	        fetchNewCaptchaImg:function () {
+		        let thisApp=this;
+		        let newAction=this.captchaType==='img'?'getNewCaptchaImg':'getNewClickCaptcha';
+		        util.ajax.get("/captchaController.php?action="+newAction+"&t="+ (new Date()).getTime())
+			        .then(function(res){
+				        if(res.data.status===200){
+					        thisApp.captchaImg=res.data.captchaImg;
+					        if(thisApp.captchaType==='click'){
+						        thisApp.clickCaptchaTextArr=res.data.captchaText;
+						        thisApp.clickCaptchaBarColor=thisApp.clickCaptchaBarColorOri;
+						        thisApp.clickCaptchaInfo=[];
+						        thisApp.clickCaptchaTextColor=thisApp.clickCaptchaTextArr.map(function (v,i) {
+							        return '#000';
+						        });
+					        }
+					        thisApp.formItem.captcha='';
+					        thisApp.updateCaptchaValue();
+					        thisApp.$store.commit('updateCaptchaData',thisApp.captchaType==='img'?{
+						        captchaImg:res.data.captchaImg,
+						        captchaText:[],
+					        }:{
+						        captchaImg:res.data.captchaImg,
+						        captchaText:res.data.captchaText,
+					        });
+				        }
+			        })
+			        .catch(function(err){
+				        console.log(err);
+			        })
+	        },
+	        handleCaptchaClick:function (e) {
+		        if(this.clickCaptchaTextArr.length===this.clickCaptchaInfo.length){
+			        return ;
+		        }
+		        let clickContainer=this.$refs['click-captcha-container'];
+		        let hOffsetTop=function( elem ){
+			        let top = elem.offsetTop;
+			        let parent = elem.offsetParent;
+			        while(parent){
+				        top += parent.offsetTop;
+				        parent = parent.offsetParent;
+			        }
+			        return top;
+		        };
+		        let hOffsetLeft=function( elem ){
+			        let left = elem.offsetLeft;
+			        let parent = elem.offsetParent;
+			        while( parent){
+				        left += parent.offsetLeft;
+				        parent = parent.offsetParent;
+			        }
+			        return left;
+		        };
+		        let scrollLeft = document.body.scrollLeft || (document.documentElement && document.documentElement.scrollLeft);
+		        let scrollTop = document.body.scrollTop || (document.documentElement && document.documentElement.scrollTop);
+		        this.clickCaptchaInfo.push((scrollLeft + e.clientX - hOffsetLeft(clickContainer)) + ',' + (scrollTop + e.clientY - hOffsetTop(clickContainer)));
+		        let cLength=this.clickCaptchaInfo.length;
+		        this.clickCaptchaTextColor=this.clickCaptchaTextArr.map(function (v,i) {
+			        return i<cLength?'#fff':'#000';
+		        });
+		        if(this.clickCaptchaTextArr.length===cLength){
+			        let postParams={
+				        info:[this.clickCaptchaInfo.join('-'), clickContainer.clientWidth, clickContainer.clientHeight].join(';')
+			        };
+
+			        let thisApp=this;
+			        util.ajax.post("/captchaController.php?action=checkClickCaptcha&t="+ (new Date()).getTime(),qs.stringify(postParams) ,{
+				        headers: {
+					        'Content-Type': 'application/x-www-form-urlencoded',
+				        },
+			        })
+				        .then(function(res){
+					        if(res.data.status===200){
+						        if(!res.data.checked){
+							        thisApp.clickCaptchaBarColor=thisApp.clickCaptchaBarColorFailure;
+							        thisApp.fetchNewCaptchaImg();
+						        }else{
+							        thisApp.formItem.captcha=postParams.info;
+							        thisApp.updateCaptchaValue();
+							        thisApp.clickCaptchaBarColor=thisApp.clickCaptchaBarColorSuccess;
+						        }
+					        }
+				        })
+				        .catch(function(err){
+					        console.log(err);
+				        })
+		        }
+	        }
+        }
+    }
+</script>
